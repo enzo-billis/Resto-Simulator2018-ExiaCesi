bdc1d335cb37828e664c45099a62d7e3f7798b61
Unknown <antoine.morlet@viacesi.fr>
2018-12-06 17:01:19 +0100

    [ADD] ThreadPool Classes - Kitchen and Salle

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/SallePools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RestaurantSimulator.Model.Salle
+{
+    class SallePools
+    {
+        //public static List<ThreadPool> ThreadPoolRankChief;
+        //public static List<ThreadPool> ThreadPoolRankChief;
+        public static List<Thread> ThreadPoolHotelMaster;
+        public static List<Thread> ThreadPoolCommis;
+
+
+
+    }
+}
