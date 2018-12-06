ec1d649a3a6834923b4628fbe4d87e4c85b2137b
Unknown <arthur.delamare1@viacesi.fr>
2018-12-06 21:11:14 +0100

    [FIX] Interface ON Waiter Class

--- RestaurantSimulator/RestaurantSimulator/Model/Salle/Characters/Waiter.cs
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/Characters/Waiter.cs
@@ -1,4 +1,5 @@
 ﻿using Restaurant.Model.Salle.Movement;
+using RestaurantSimulator.Model.Salle.Movement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,17 @@ using System.Threading.Tasks;
 
 namespace RestaurantSimulator.Model.Salle.Characters
 {
-    public class Waiter : Position
+    public class Waiter : Position, IMove
     {
         public Waiter(int posX, int posY) : base(posX, posY)
         {
         }
         public Waiter() : base() { }
+
+        public void Move(int posX, int posY)
+        {
+            this.PosX = posX;
+            this.PosY = posY;
+        }
     }
 }
