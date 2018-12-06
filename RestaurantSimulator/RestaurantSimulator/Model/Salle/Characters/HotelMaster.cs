8227a98a709546949a87fd8bc815b136494d41e2
Unknown <arthur.delamare1@viacesi.fr>
2018-12-06 17:23:32 +0100

    [ADD] HotelMaster AND Empty RankChief

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/Characters/HotelMaster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Model.Salle.Characters
+{
+    public class HotelMaster
+    {
+        private List<RankChief> rankChief;
+
+        public List<RankChief> RankChief
+        {
+            get => rankChief;
+            set => rankChief = value;
+        }
+    }
+}
