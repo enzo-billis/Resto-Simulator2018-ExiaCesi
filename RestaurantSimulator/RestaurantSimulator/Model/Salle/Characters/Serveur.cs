0dfe28d038d1cd3d82d8ad992000d23956c48f0c
ZENBOY <gabriel.lesourd@viacesi.fr>
2018-12-06 17:01:31 +0100

    Add Serveur
    Class

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/Characters/Serveur.cs
@@ -0,0 +1,17 @@
+﻿using Restaurant.Model.Salle.Movement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Model.Salle.Characters
+{
+    class Serveur : Position
+    {
+        public Serveur(int posX, int posY) : base(posX, posY)
+        {
+        }
+        public Serveur() : base() { }
+    }
+}
