0a73a62b90590671cb74eb0832f8dd08b4072dcf
Unknown <antoine.morlet@viacesi.fr>
2018-12-07 14:12:45 +0100

    [ADD] Model Timer

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Shared/Timer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Model.Shared
+{
+    public static class Timer
+    {
+        private static double time;
+        public static double Time { get => time; set => time = value; }
+    }
+}
