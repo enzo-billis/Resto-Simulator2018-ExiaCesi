8950e5855e19a4775633c7c5cabd73fc47a7bed9
Unknown <arthur.delamare1@viacesi.fr>
2018-12-07 12:57:33 +0100

    [ADD] AbstractClientFactory Class

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/Factory/AbstractClientFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantSimulator.Model.Salle.Characters;
+
+namespace RestaurantSimulator.Model.Salle.Factory
+{
+    public abstract class AbstractClientFactory : IClientFactory
+    {
+        public abstract Client CreateClient();
+    }
+}
