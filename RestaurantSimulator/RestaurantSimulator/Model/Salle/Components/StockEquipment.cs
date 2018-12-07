329fd160cc15e2ed0dea2aa742c7882900519f70
Enzo BILLIS <enzo.billis@viacesi.fr>
2018-12-07 10:41:08 +0100

    [ADD] Stock classes & test's for Singleton

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/Components/StockEquipment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Model.Salle.Components
+{
+    public class StockEquipment
+    {
+        private Dictionary<string, int> clean;
+        private Dictionary<string, int> dirty;
+        private static StockEquipment instance;
+        private static readonly object padlock = new object();
+
+        public Dictionary<string, int> Clean { get => clean; set => clean = value; }
+        public Dictionary<string, int> Dirty { get => dirty; set => dirty = value; }
+
+        public static StockEquipment Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new StockEquipment();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        private StockEquipment()
+        {
+            this.clean = new Dictionary<string, int>();
+            this.dirty = new Dictionary<string, int>();
+        }
+
+        
+    }
+}
