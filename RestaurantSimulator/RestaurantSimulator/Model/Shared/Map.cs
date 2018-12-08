82431487eb810346052d689e0250666944fc0736
Unknown <arthur.delamare1@viacesi.fr>
2018-12-08 19:21:54 +0100

    [ADD] Test BDD

---
+++ RestaurantSimulator/RestaurantSimulator/Model/Shared/Map.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantSimulator.Model.Shared
+{
+    public class Map
+    {
+        private static Map instance;
+        private List<Recette> recipes;
+
+        public static Map Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Map();
+                return instance;
+            }
+        }
+
+        private Map()
+        {
+            this.recipes = new List<Recette>();
+        }
+
+        public List<Recette> Recipes { get => recipes; set => recipes = value; }
+    }
+}
