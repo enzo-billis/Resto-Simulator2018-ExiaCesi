183f4a877bda18c3d64f71e371c1faba6ed64e9a
Unknown <arthur.delamare1@viacesi.fr>
2018-12-08 23:24:14 +0100

    [ADD] Square Test Class

---
+++ RestaurantSimulator/RestoTests/Model/Cuisine/Components/TestSquare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantSimulator.Model.Salle.Components;
+using RestaurantSimulator.Model.Shared;
+
+namespace RestoTests.Model.Cuisine.Components
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void TestSquareConstruct()
+        {
+            Square square = new Square();
+            Assert.IsNotNull(square.Tables);
+            Assert.IsNotNull(square.Waiters);
+            Assert.AreEqual(Parameters.TABLES_BY_SQUARE, square.Tables.Count);
+            Assert.AreEqual(Parameters.WAITER_BY_SQUARE, square.Waiters.Count);
+        }
+    }
+}
