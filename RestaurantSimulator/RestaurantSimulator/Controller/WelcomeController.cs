70d9ce82e74dd1624aca95592fd4312b1386c095
Unknown <arthur.delamare1@viacesi.fr>
2018-12-08 22:44:58 +0100

    [ADD] WelcomeController : GenerateClient AND CreateGroup

---
+++ RestaurantSimulator/RestaurantSimulator/Controller/WelcomeController.cs
@@ -0,0 +1,58 @@
+﻿using Restaurant.Model.Shared;
+using RestaurantSimulator.Model.Salle.Characters;
+using RestaurantSimulator.Model.Salle.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Controller
+{
+    public class WelcomeController
+    {
+        private HotelMaster hotelMaster;
+
+        public WelcomeController(HotelMaster hotelMaster)
+        {
+            this.hotelMaster = hotelMaster;
+        }
+
+        public Client GenerateClient()
+        {
+            Client client;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+            switch (randomNumber)
+            {
+                case int rn when (rn <= 20):
+                    client = HurriedClientFactory.Instance.CreateClient();
+                    break;
+
+                case int rn when (rn > 20 && rn < 80):
+                    client = NormalClientFactory.Instance.CreateClient();
+                    break;
+
+                case int rn when (rn >= 80 && rn <= 100):
+                    client = CoolClientFactory.Instance.CreateClient();
+                    break;
+
+                default:
+                    client = NormalClientFactory.Instance.CreateClient();
+                    break;
+            }
+
+            return client;
+        }
+
+        public Group CreateGroup(int clientNumber)
+        {
+            Group group = new Group();
+            for (int i = 0; i < clientNumber; i++)
+            {
+                group.Clients.Add(this.GenerateClient());
+            }
+            return group;
+        }
+    }
+}
---
+++ RestaurantSimulator/RestoTests/Controller/TestWelcomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Restaurant.Model.Shared;
+using RestaurantSimulator.Controller;
+using RestaurantSimulator.Model.Salle.Characters;
+
+namespace RestoTests.Controller
+{
+    [TestClass]
+    public class TestWelcomeController
+    {
+        HotelMaster hotelMaster;
+        WelcomeController welcomeController;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            hotelMaster = new HotelMaster();
+            welcomeController = new WelcomeController(hotelMaster);
+        }
+
+        [TestMethod]
+        public void TestGenerateClient()
+        {
+            List<Client> clients = new List<Client>();
+            for (int i = 0; i < 100000; i++)
+            {
+                clients.Add(welcomeController.GenerateClient());
+                Assert.IsNotNull(clients[i]);
+                Assert.IsInstanceOfType(clients[i], typeof(Client));
+            }
+            Assert.IsTrue(clients.Exists(client => client.Strategy.ContainsValue(0)));
+            Assert.IsTrue(clients.Exists(client => client.Strategy.ContainsValue(1)));
+            Assert.IsTrue(clients.Exists(client => client.Strategy.ContainsValue(2)));
+        }
+
+        [TestMethod]
+        public void TestCreateGroup()
+        {
+            Group group = welcomeController.CreateGroup(4);
+            Assert.AreEqual(4, group.Clients.Count);
+        }
+    }
+}
