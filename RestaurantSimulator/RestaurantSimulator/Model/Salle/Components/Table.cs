54cc37a98cea3755914c2b46daac33450308b2bf
Unknown <arthur.delamare1@viacesi.fr>
2018-12-09 16:53:54 +0100

    [FIX] Table: PosX, PosY properties AND Constructor

--- RestaurantSimulator/RestaurantSimulator/Model/Salle/Components/Table.cs
+++ RestaurantSimulator/RestaurantSimulator/Model/Salle/Components/Table.cs
@@ -1,4 +1,5 @@
 ﻿using Restaurant.Model.Salle.Components;
+using Restaurant.Model.Salle.Movement;
 using Restaurant.Model.Shared;
 using System;
 using System.Collections.Generic;
@@ -8,17 +9,26 @@ using System.Threading.Tasks;
 
 namespace RestaurantSimulator.Model.Salle.Components
 {
-    public class Table : Equipment
+    public class Table : Equipment, IPosition
     {
         private int nbPlaces;
         private Group group;
         private bool entree = false;
         private bool plate = false;
         private bool dessert = false;
+        private int posX = 0;
+        private int posY = 0;
 
         public Table(int nbPlaces)
         {
             this.nbPlaces = nbPlaces;
+        }
+
+        public Table(int nbPlaces, int posX, int posY)
+        {
+            this.nbPlaces = nbPlaces;
+            this.posX = posX;
+            this.posY = posY;
         }
 
         public int NbPlaces { get => nbPlaces; set => nbPlaces = value; }
@@ -26,5 +36,7 @@ namespace RestaurantSimulator.Model.Salle.Components
         public bool Entree { get => entree; set => entree = value; }
         public bool Plate { get => plate; set => plate = value; }
         public bool Dessert { get => dessert; set => dessert = value; }
+        public int PosX { get => posX; set => posX = value; }
+        public int PosY { get => posY; set => posY = value; }
     }
 }
