1147e3b431f26655f6a0ec2bcbe7065c56db6a96
Unknown <arthur.delamare1@viacesi.fr>
2018-12-09 18:51:33 +0100

    [ADD] BDDController Class / Test : ConsumeIngredient Method

---
+++ RestaurantSimulator/RestaurantSimulator/Controller/BDDController.cs
@@ -0,0 +1,57 @@
+﻿using RestaurantSimulator.Model.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Controller
+{
+    public class BDDController
+    {
+        private static BDDController instance;
+
+        private BDDController()
+        {
+            this.InitConnection();
+        }
+
+        public static BDDController Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BDDController();
+                return instance;
+            }
+        }
+
+        public BDDRestaurant DB;
+
+        public void InitConnection()
+        {
+            DB = new BDDRestaurant();
+        }
+
+        public void CloseConnection()
+        {
+            DB.Dispose();
+        }
+
+        public void ConsumeIngredient(Ingredient ingredient)
+        {
+            if(ingredient != null)
+            {
+                if(DB.Ingredient.Find(ingredient.id_Ingredient) != null)
+                {
+                    var result = DB.Stock.SingleOrDefault(stock => stock.id_Ingredient == ingredient.id_Ingredient);
+                    if (result.quantité_Stock > 0)
+                    {
+                        result.quantité_Stock--;
+                    }
+                    DB.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ RestaurantSimulator/RestoTests/Controller/TestBDDController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantSimulator.Controller;
+using System.Linq;
+using RestaurantSimulator.Model.Shared;
+
+namespace RestoTests.Controller
+{
+    [TestClass]
+    public class TestBDDController
+    {
+
+        [TestMethod]
+        public void TestConsumeIngredient()
+        {
+            var DB = BDDController.Instance.DB;
+            var stockIngredient = from stock in DB.Stock
+                                  where stock.id_Ingredient == 5
+                                  select stock;
+
+            var ingredientFromStock = stockIngredient.FirstOrDefault<Stock>();
+            var ingredientFromIngredient = DB.Ingredient.Single(ing => ing.id_Ingredient == 5);
+            int oldQuantity = (int)ingredientFromStock.quantité_Stock;
+            BDDController.Instance.ConsumeIngredient(ingredientFromIngredient);
+
+            Assert.AreEqual(oldQuantity - 1,
+                DB.Stock.Single(ing => ing.id_Ingredient == 5).quantité_Stock);
+
+            ingredientFromStock.quantité_Stock++;
+            DB.SaveChanges();
+        }
+    }
+}
