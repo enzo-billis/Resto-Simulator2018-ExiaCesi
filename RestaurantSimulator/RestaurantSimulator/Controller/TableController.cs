dbc837eec98fe60f448adf89f2a11cd1bbeb4aac
Unknown <arthur.delamare1@viacesi.fr>
2018-12-09 13:02:48 +0100

    [ADD] RestaurantController: FindOptimizedTable AND AttributionTableGroup

---
+++ RestaurantSimulator/RestaurantSimulator/Controller/TableController.cs
@@ -0,0 +1,43 @@
+﻿using Restaurant.Model.Salle.Components;
+using Restaurant.Model.Shared;
+using RestaurantSimulator.Model.Salle.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulator.Controller
+{
+    public class TableController
+    {
+        public Table OptimizedFindTable(List<Table> tables, int groupSize)
+        {
+            if (groupSize <= 10)
+            {
+                int i = groupSize;
+                while (i < 10)
+                {
+                    if (tables.Exists(table => table.NbPlaces == i))
+                        return tables.Find(table => table.NbPlaces == i);
+                    i++;
+                }
+                
+            }
+            return null;
+        }
+
+        public bool AttributionTableGroup(Group group, Table table)
+        {
+            if((table.State == EquipmentState.Available) 
+                && (table.NbPlaces >= group.Clients.Count))
+            {
+                table.Group = group;
+                table.State = EquipmentState.InUse;
+                group.State = GroupState.Ordering;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ RestaurantSimulator/RestoTests/Controller/TestTableController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Restaurant.Model.Salle.Components;
+using Restaurant.Model.Shared;
+using RestaurantSimulator.Controller;
+using RestaurantSimulator.Model.Salle.Characters;
+using RestaurantSimulator.Model.Salle.Components;
+
+namespace RestoTests.Controller
+{
+    [TestClass]
+    public class TestTableController
+    {
+        [TestMethod]
+        public void TestAttributionTableGroup()
+        {
+            HotelMaster hotelMaster = new HotelMaster();
+            WelcomeController welcomeController = new WelcomeController(hotelMaster);
+            Group group = welcomeController.CreateGroup(5);
+            TableController tableController = new TableController();
+            Table table = tableController.OptimizedFindTable(hotelMaster.RankChiefs[0].Squares[0].Tables, group.Clients.Count);
+            if (table == null)
+                table = tableController.OptimizedFindTable(hotelMaster.RankChiefs[1].Squares[0].Tables, group.Clients.Count);
+            Assert.IsNotNull(table);
+            bool success = tableController.AttributionTableGroup(group, table);
+            Assert.IsTrue(success);
+            Assert.AreEqual(EquipmentState.InUse, table.State);
+            Assert.AreEqual(GroupState.Ordering, group.State);
+        }
+    }
+}
