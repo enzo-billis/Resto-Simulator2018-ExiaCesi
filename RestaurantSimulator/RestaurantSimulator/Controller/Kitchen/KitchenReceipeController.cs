1ef8b63365713e201dafea783a4cd611c9357e74
Enzo BILLIS <enzo.billis@viacesi.fr>
2018-12-10 15:08:27 +0100

    Add kitchenReceipe method to verify tool dispo of a receipe

--- RestaurantSimulator/RestaurantSimulator/Controller/Kitchen/KitchenReceipeController.cs
+++ RestaurantSimulator/RestaurantSimulator/Controller/Kitchen/KitchenReceipeController.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestaurantSimulator.Model.Shared;
+using System.Text.RegularExpressions;
+using RestaurantSimulator.Controller;
+using RestaurantSimulator.Controller.Kitchen;
 
 namespace RestaurantSimulator.Controller.Kitchen
 {
     public class KitchenReceipeController
     {
+        public void CookCallback(Recette receipe)
+        {
+
+        }
+
+        public void GetReceipe(Recette receipe)
+        {
+            
+        }
+
+        public bool VerifyDispoTool(Recette receipe)
+        {
+            KitchenToolsController kitchenToolsController = new KitchenToolsController();
+
+            string[] steps = Regex.Split(receipe.liste_etapes_recette, ";");
+            foreach (var step in steps)
+            {
+                int actualStepID = Convert.ToInt32(step);
+
+                try
+                {
+                    Etape actualStep = BDDController.Instance.DB.Etape.SingleOrDefault(r => r.id_etape == actualStepID);
+                    Ustensile actualStepTool = BDDController.Instance.DB.Ustensile.SingleOrDefault(r => r.id_Ustensile == actualStep.id_Ustensile);
+                    string toolName = actualStepTool.nom_ust_Ustensile;
+
+                    if (kitchenToolsController.VerifyStock(toolName, 1))
+                    {
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+
+        public void FreeSemaphReceipe()
+        {
+            
+        }
     }
 }
---
+++ RestaurantSimulator/RestoTests/Controller/TestKitchenReceipeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantSimulator.Model.Shared;
+using RestaurantSimulator.Controller;
+using System.Linq;
+using RestaurantSimulator.Controller.Kitchen;
+
+namespace RestoTests.Controller
+{
+    /// <summary>
+    /// Description résumée pour TestKitchenReceipeController
+    /// </summary>
+    [TestClass]
+    public class TestKitchenReceipeController
+    {
+        [TestMethod]
+        public void CookCallbackTest()
+        {
+            //
+            // TODO: ajoutez ici la logique du test
+            //
+        }
+        [TestMethod]
+        public void GetReceipeTest()
+        {
+            //
+            // TODO: ajoutez ici la logique du test
+            //
+        }
+        [TestMethod]
+        public void VerifyDispoToolTest()
+        {
+            KitchenReceipeController controllerReceipe = new KitchenReceipeController();
+
+            Recette receipe = BDDController.Instance.DB.Recette.SingleOrDefault(r => r.id_recette == 1);
+            Assert.IsTrue(controllerReceipe.VerifyDispoTool(receipe));
+
+            receipe = BDDController.Instance.DB.Recette.SingleOrDefault(r => r.id_recette == 2);
+            Assert.IsFalse(controllerReceipe.VerifyDispoTool(receipe));
+
+            receipe = BDDController.Instance.DB.Recette.SingleOrDefault(r => r.id_recette == 0);
+            Assert.IsFalse(controllerReceipe.VerifyDispoTool(receipe));
+        }
+        [TestMethod]
+        public void FreeSemaphReceipeTest()
+        {
+            //
+            // TODO: ajoutez ici la logique du test
+            //
+        }
+    }
+}
