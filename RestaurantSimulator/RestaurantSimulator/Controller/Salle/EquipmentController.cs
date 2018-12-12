e694ec5bfd7950598bd57a9ed29f3921647dc36c
Enzo BILLIS <enzo.billis@viacesi.fr>
2018-12-12 15:05:39 +0100

    Add equipment controller & tests with 4 dictionary

---
+++ RestaurantSimulator/RestaurantSimulator/Controller/Salle/EquipmentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantSimulator.Model.Salle.Components;
+
+namespace RestaurantSimulator.Controller.Salle
+{
+    public static class EquipmentController
+    {
+        public static bool defineAsDirty(string elem)
+        {
+            bool existDirty = StockEquipment.Instance.Dirty.TryGetValue(elem, out int eNumberDirty);
+            bool existInUse = StockEquipment.Instance.InUse.TryGetValue(elem, out int eNumberInUse);
+            if ((existDirty) && (existInUse) && (eNumberInUse > 0))
+            {
+                StockEquipment.Instance.InUse[elem] = eNumberInUse - 1;
+                StockEquipment.Instance.Dirty[elem] = eNumberDirty + 1;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool defineAsInUse(string elem)
+        {
+            bool existClean = StockEquipment.Instance.Clean.TryGetValue(elem, out int eNumberClean);
+            bool existInUse = StockEquipment.Instance.InUse.TryGetValue(elem, out int eNumberInUse);
+            if ((existClean) && (existInUse) && (eNumberClean > 0))
+            {
+                StockEquipment.Instance.InUse[elem] = eNumberInUse + 1;
+                StockEquipment.Instance.Clean[elem] = eNumberClean - 1;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool defineAsClean(string elem)
+        {
+            bool existWashing = StockEquipment.Instance.Washing.TryGetValue(elem, out int eNumberWashing);
+            bool existClean = StockEquipment.Instance.Clean.TryGetValue(elem, out int eNumberClean);
+            if ((existClean) && (existWashing) && (eNumberWashing > 0))
+            {
+                StockEquipment.Instance.Clean[elem] = eNumberClean + 1;
+                StockEquipment.Instance.Washing[elem] = eNumberWashing - 1;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool defineAsWashing(string elem)
+        {
+            bool existWashing = StockEquipment.Instance.Washing.TryGetValue(elem, out int eNumberWashing);
+            bool existDirty = StockEquipment.Instance.Dirty.TryGetValue(elem, out int eNumberDirty);
+            if ((existDirty) && (existWashing) && (eNumberDirty > 0))
+            {
+                StockEquipment.Instance.Washing[elem] = eNumberWashing + 1;
+                StockEquipment.Instance.Dirty[elem] = eNumberDirty - 1;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ RestaurantSimulator/RestoTests/Controller/TestCleanerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestoTests.Controller
+{
+    /// <summary>
+    /// Description résumée pour TestCleanerController
+    /// </summary>
+    [TestClass]
+    public class TestCleanerController
+    {
+        
+        [TestMethod]
+        public static void TestMethod1()
+        {
+            //
+            // TODO: ajoutez ici la logique du test
+            //
+        }
+    }
+}
